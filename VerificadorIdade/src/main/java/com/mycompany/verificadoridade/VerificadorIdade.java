d467825c7b196e06abb8e0f93b86ea0c1fb97545
marcosbarbosa <164069306+marcosbarbosa42@users.noreply.github.com>
2024-07-15 16:50:50 -0300

    Verificador de idade

---
+++ VerificadorIdade/src/main/java/com/mycompany/verificadoridade/VerificadorIdade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.verificadoridade;
+
+/**
+ *
+ * @author datasus
+ */
+public class VerificadorIdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
