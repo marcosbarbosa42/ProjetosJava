ac1aca4e397d45e763438151498a9dba163901fc
marcosbarbosa <164069306+marcosbarbosa42@users.noreply.github.com>
2024-07-30 17:36:20 -0300

    programa par

---
+++ ProgramaPar/src/main/java/com/mycompany/programapar/ProgramaPar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programapar;
+
+/**
+ *
+ * @author datasus
+ */
+public class ProgramaPar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
