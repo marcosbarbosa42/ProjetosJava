f5e53ba6b3a8d140d73355954ca2d678fefaef4b
marcosbarbosa <164069306+marcosbarbosa42@users.noreply.github.com>
2024-08-07 14:27:50 -0300

    programavoto

---
+++ ProgramaVoto/src/main/java/com/mycompany/programavoto/ProgramaVoto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programavoto;
+
+/**
+ *
+ * @author datasus
+ */
+public class ProgramaVoto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
