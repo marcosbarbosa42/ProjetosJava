205ccc5c04157e34c5ced74662312c518b77ed79
marcosbarbosa <164069306+marcosbarbosa42@users.noreply.github.com>
2024-08-23 15:00:59 -0300

    repeticao

---
+++ Contador01/src/main/java/com/mycompany/contador01/Contador01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contador01;
+
+/**
+ *
+ * @author datasus
+ */
+public class Contador01 {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        while(cc<6){
+            
+            System.out.println("Cambalhota "+ cc);
+            cc = cc + 1;
+        }
+    }
+}
