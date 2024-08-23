9068e3fe1815df2e6be22f835f697247a78969a7
marcosbarbosa <164069306+marcosbarbosa42@users.noreply.github.com>
2024-08-23 15:02:09 -0300

    repeticao

---
+++ Repita01/src/main/java/com/mycompany/repita01/Repita01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repita01;
+
+/**
+ *
+ * @author datasus
+ */
+public class Repita01 {
+
+    public static void main(String[] args) {
+      int cc = 0;
+      do {
+          System.out.println("Cambalhota "+ cc);
+          cc++;
+          
+      }while (cc<4);
+    }
+}
